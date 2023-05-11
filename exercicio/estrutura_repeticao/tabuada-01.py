d1fd093e2f69a8e1961eb9445451b5cc6d04278d
willian aquino <willian_wjunior@hotmail.com>
2023-05-11 09:59:37 -0300

    .

---
+++ exercicio/estrutura_repeticao/tabuada-01.py
@@ -0,0 +1,9 @@
+tabuada = 1
+
+while (tabuada <= 10):
+    print('Tabuada do {}'.format(tabuada))
+    i = 1
+    while (i <= 10):
+        print('{} x {} = {} '.format(tabuada, i, tabuada * i))
+        i += 1
+    tabuada += 1
