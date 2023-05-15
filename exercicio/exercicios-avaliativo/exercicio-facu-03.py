9896aa62a12847f8153ed5f8d68115dba2cefa4e
willian aquino <willian_wjunior@hotmail.com>
2023-05-14 23:37:08 -0300

    2.0

--- exercicio/exercicios-avaliativo/exercicio-facu-03.py
+++ exercicio/exercicios-avaliativo/exercicio-facu-03.py
@@ -2,31 +2,29 @@ print("Bem vindo a companhia de Logistica Willian aquino")
 
 
 def dimensoesObejeto():
-    volume = 0
-   
-    try:
-        comprimento = int(input('Digite o comprimento do obejto (em cm)'))
-        largura = int(input('Digite a largura do obejto (em cm)'))
-        altura = int(input('Digite a altura do obejto (em cm)'))
-        volume = (altura * largura * comprimento)
-
-
-    except ValueError:
-        print("Você digitou alguma dimensão não numérico")
-    except:
-        print("algo deu errado")
-        return
-
+    while True:
+        try:
+            comprimento = int(input('Digite o comprimento do obejto (em cm)'))
+            largura = int(input('Digite a largura do obejto (em cm)'))
+            altura = int(input('Digite a altura do obejto (em cm)'))
+            volume = (altura * largura * comprimento)
+            break
+        except ValueError:
+            print("Você digitou alguma dimensão não numérico")
+        except:
+            print("algo deu errado")
     if (volume < 1000):
         return 10
     elif (volume >= 1000 and volume < 10000):
         return 20
     elif (volume >= 10000 and volume < 30000):
         return 30
-    elif (volume >= 30000 and volume < 10000):
+    elif (volume >= 30000 and volume < 100000):
         return 50
     elif (volume >= 100000):
         print("Não é aceito objetos tão grande")
+
+    print("o volume do objeto é {}".format(volume))
 
 
 def pesoObjeto():
@@ -74,13 +72,4 @@ def rotaObjeto():
             print("Por favor, digite a rota desejada novamente.")
 
 
-dimensoesObejeto()
-pesoObjeto()
-rotaObjeto()
-
-
-
-
-
-
-
+print(dimensoesObejeto())
